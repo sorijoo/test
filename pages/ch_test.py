f5cfa3f5804db2b4f0153e336b084c5bd3289f1e
sorijoo <sorij95@gmail.com>
2022-07-22 21:55:18 +0900

    test

--- pages/ch_test.py
+++ pages/ch_test.py
@@ -17,35 +17,7 @@ labels.remove("Z")
 
 def process(frame):
     frame.flags.writeable = True
-    # frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-    imgOutput = frame.copy()
     hands, frame = detector.findHands(frame)
-    if hands:
-        x, y, w, h = hands[0]['bbox']
-        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8)*255
-        imgCrop = img[y-offset:y+h+offset, x-offset:x+w+offset]
-        aspectRatio = h/w
-        if aspectRatio>1:
-            k = imgSize/h
-            wCal = math.ceil(k*w)
-            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
-            imgResizeShape = imgResize.shape
-            wGap = math.ceil((imgSize-wCal)/2)
-            imgWhite[:, wGap:wCal+wGap] = imgResize
-            prediction, index = classifier.getPrediction(imgWhite, draw=False)
-            # print(prediction, index)
-        else:
-            k = imgSize/w
-            hCal = math.ceil(k*h)
-            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
-            imgResizeShape = imgResize.shape
-            hGap = math.ceil((imgSize-hCal)/2)
-            imgWhite[hGap:hCal+hGap, :] = imgResize
-            prediction, index = classifier.getPrediction(imgWhite, draw=False)
-        cv2.rectangle(imgOutput, (x-offset, y-offset-50), (x-offset+90, y-offset-50+50), (255, 0, 139), cv2.FILLED)
-        cv2.putText(imgOutput, labels[index], (x, y-26), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 255, 255), 2)
-        cv2.rectangle(imgOutput, (x-offset, y-offset), (x+w+offset, y+h+offset), (255, 0, 139), 4)
-    
     return cv2.imshow(imgOutput)
 
 RTC_CONFIGURATION = RTCConfiguration(
@@ -54,7 +26,6 @@ RTC_CONFIGURATION = RTCConfiguration(
 
 class VideoProcessor:
     def recv(self, frame):
-        # img = frame.to_ndarray(format="bgr24")
         img = process(img)
         return av.VideoFrame.from_ndarray(img)
 
