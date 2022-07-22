e80df2eaa017c84a3bd87b2066b25ab51661f698
sorijoo <sorij95@gmail.com>
2022-07-22 21:49:06 +0900

    test

--- pages/ch_test.py
+++ pages/ch_test.py
@@ -21,27 +21,31 @@ def process(frame):
     imgOutput = frame.copy()
     hands, frame = detector.findHands(frame)
     if hands:
-        x, y, w, h = hands[0]["bbox"]
+        x, y, w, h = hands[0]['bbox']
         imgWhite = np.ones((imgSize, imgSize, 3), np.uint8)*255
-        imgCrop = frame[y-offset:y+h+offset, x-offset:x+w+offset]
-        aspectRatio =h/w
+        imgCrop = img[y-offset:y+h+offset, x-offset:x+w+offset]
+        aspectRatio = h/w
         if aspectRatio>1:
             k = imgSize/h
             wCal = math.ceil(k*w)
             imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+            imgResizeShape = imgResize.shape
             wGap = math.ceil((imgSize-wCal)/2)
-            predition, idx = classifier.getPrediction(imgWhite, draw=False)
+            imgWhite[:, wGap:wCal+wGap] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
+            # print(prediction, index)
         else:
             k = imgSize/w
             hCal = math.ceil(k*h)
             imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+            imgResizeShape = imgResize.shape
             hGap = math.ceil((imgSize-hCal)/2)
             imgWhite[hGap:hCal+hGap, :] = imgResize
-            predition, idx = classifier.getPrediction(imgWhite, draw=False)
-            cv2.rectangle(imgOutput, (x-offset, y-offset-50), (x-offset+90, y-offset-50+50), (255, 0, 139), cv2.FILLED)
-            cv2.putText(imgOutput, labels[idx], (x, y-26), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 255, 255), 2)
-            cv2.rectangle(imgOutput, (x-offset, y-offset), (x+w+offset, y+h+offset), (255, 0, 139), 4)
-
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
+        cv2.rectangle(imgOutput, (x-offset, y-offset-50), (x-offset+90, y-offset-50+50), (255, 0, 139), cv2.FILLED)
+        cv2.putText(imgOutput, labels[index], (x, y-26), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 255, 255), 2)
+        cv2.rectangle(imgOutput, (x-offset, y-offset), (x+w+offset, y+h+offset), (255, 0, 139), 4)
+    
     return cv2.imshow(imgOutput)
 
 RTC_CONFIGURATION = RTCConfiguration(
@@ -50,9 +54,9 @@ RTC_CONFIGURATION = RTCConfiguration(
 
 class VideoProcessor:
     def recv(self, frame):
-        img = frame.to_ndarray(format="bgr24")
+        # img = frame.to_ndarray(format="bgr24")
         img = process(img)
-        return av.VideoFrame.from_ndarray(img, format="bgr24")
+        return av.VideoFrame.from_ndarray(img)
 
 webrtc_ctx = webrtc_streamer(
     key="WYH",
