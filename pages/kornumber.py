154a18b60f449b9a69e66fc7a9a2884a6ef4669d
sorijoo <sorij95@gmail.com>
2022-07-22 20:37:27 +0900

    test

--- pages/kornumber.py
+++ pages/kornumber.py
@@ -1,10 +1,6 @@
 import streamlit as st 
-
 import tensorflow as tf
-
 import numpy as np
-
-
 from PIL import Image
 
 
@@ -22,15 +18,20 @@ SIGN
 """)
 filename = st.file_uploader("Choose a file")
 
-st.text(type(filename))
 
 model = tf.keras.models.load_model('model/model_kor_num_no_augmentation.h5')
 
-# idx = [x for x in range(0, 24)]
-# alpha = [chr(x).upper() for x in range(97, 123)]
-
-# alpha.remove("J")
-# alpha.remove("Z")
+convertDict = {0: "1",
+ 1: "10",
+ 2: "10",
+ 3: "2",
+ 4: "3",
+ 5: "4",
+ 6: "5",
+ 7: "6",
+ 8: "7",
+ 9: "8",
+ 10: "9"}
 
 
 
@@ -43,22 +44,7 @@ def convert_letter(result):
         return "err"
 
 
-def img_resize_to_gray(filename):
-    """파일 경로를 입력 받아 사이즈 조정과 그레이로 변환하는 함수
-
-    Args:
-        filename (str): 파일 경로
-    Returns:
-        arr (np.array)
-    """
-    img = Image.open(filename)
-    img = img.convert('RGB')
-    img = img.resize((300, 300))
-    return img
-
 if filename is not None:
-    st.text(filename)
-    st.text(type(filename))
     img = Image.open(filename)
     img = img.convert('L')
     img = img.resize((300, 300)) 
@@ -66,4 +52,6 @@ if filename is not None:
     pred = np.argmax(model.predict(img.reshape(1, 300, 300, 1)))
     # text = []
     st.image(img, use_column_width=False)
-    st.text(pred)
+    st.text("혹시.. 당신이 원하는 숫자가")
+    st.title(convertDict[pred])
+    st.text("인가요?")
