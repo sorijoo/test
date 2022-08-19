6a20161fc90bb006fbbb4434d694120ad45f2434
sorijoo <sorij95@gmail.com>
2022-08-19 10:20:57 +0900

    streamlit2

--- pages/2-개발 과정.py
+++ pages/2-개발 과정.py
@@ -187,7 +187,7 @@ L2-Norm의 경우, 두 벡터가 일치하면(기존 안무와 사용자의 안�
 
 """)
 st.latex("""
-ACC = {K \over L2_pose + w \times L2_hand + K}
+ACC = {K \over L2{pose} + w \times L2{hand} + K}
 """)
 
 st.subheader('테스트 영상')
